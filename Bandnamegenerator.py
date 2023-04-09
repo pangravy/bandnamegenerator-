4fc39563bcde9f256178e04290c6f94849ce021b
pangravy <swayamsamantaray21@gmail.com>
2023-04-10 02:39:11 +0530

    first commit

---
+++ Bandnamegenerator.py
@@ -0,0 +1,11 @@
+#Band name generator
+print("Welcome to the band name generator program\nplease fill in some details to continue ahead")
+a = input("which city you grew up in when you were a child?")
+print(a)
+b = input("what is the name of your first pet?")
+print(b)
+c= input("whats the name of your favorite food?")
+print(c)
+
+print("The band name generator has come up with a name \n" + a + " " + b + " " + c)
+print("Thank you for choosing our band name generator")
